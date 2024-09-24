dc3b8b5feff41bdd0c07e3601319c75b74058a6f
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2024-09-23 21:23:40 -0700

    Add Patient class

---
+++ patient.py
@@ -0,0 +1,23 @@
+from person import Person
+from doctor import Doctor
+
+class Patient(Person): 
+  # Instances Attribute
+  def __init__(self, patient_id: int, name: str, age: int, gender: str, address: str) -> None:
+    super().__init__(name, age, gender, address)
+    self.patient_id = patient_id
+    self.medical_history = [] # List of medical records for the patient 
+    self.assigned_doctor =  Doctor # The doctor assigned to the patient (Doctor object).
+    self.appointments = [] # List of scheduled appointments (list of Appointment objects).
+    self.bills = [] # List of bills generated for the patient (list of Bill objects). 
+
+  def add_appointment(self, appointment): # Adds an appointment to the patient’s list.
+    if appointment not in self.appointments: # If the appointment is not in the list 
+      self.appointments.append(appointment)
+      return f'You have this appointment {appointment} coming up'
+    return f'This appointment {appointment} is already on your List'
+  
+  def add_medical_record(self, record): # Adds a medical record to the patient’s history.
+    self.medical_history.append(record)
+    return f'Add the record: {record} to the medical history'
+  
