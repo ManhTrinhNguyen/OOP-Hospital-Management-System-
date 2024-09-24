0ad2e44077fc08d628e764d5df8d0c446be13dc7
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2024-09-23 20:51:25 -0700

    Add Person Class

---
+++ person.py
@@ -0,0 +1,13 @@
+class Person: 
+  # Instances Attribute 
+  def __init__(self, name: str, age: int, gender: str, address: str) -> None:
+    self.name = name 
+    self.age = age 
+    self.gender = gender 
+    self.address = address 
+
+  def __str__(self) -> str: # Represent a Person 
+    return f'Name: {self.name}, Age: {self.age}, Gender: {self.gender}, Address: {self.address}'
+  
+
+    
