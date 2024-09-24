6d055892148dcc7c0f3ab1a96def550409d54a70
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2024-09-23 21:49:45 -0700

    Add text

--- medical_record.py
+++ medical_record.py
@@ -1,8 +1,10 @@
 class Medical_Record: 
   def __init__(self, record_id: int, patient, doctor, diagnosis: str, treatment: str, date) -> None:
     self.record_id = record_id
-    self.patient = patient
-    self.doctor = doctor
-    self.diagnosis = diagnosis
-    self.treatment = treatment
-    self.date = date 
+    self.patient = patient # The patient associated with the record (Patient object).
+    self.doctor = doctor # The doctor responsible for the diagnosis/treatment (Doctor object).
+    self.diagnosis = diagnosis # The diagnosis provided by the doctor (string).
+    self.treatment = treatment # The treatment prescribed (string).
+    self.date = date # Date of the diagnosis/treatment (datetime object). 
+
+
